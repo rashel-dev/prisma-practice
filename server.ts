34d98659fb41243dc252f5b094518f1613ecd161
rashel9255 <ibrahim2000167@gmail.com>
2026-05-26 00:26:26 +0600

    create first user in the neon

--- server.ts
+++ server.ts
@@ -1,5 +1,13 @@
+import "dotenv/config";
 import express from "express"
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 const app = express()
 
 app.get("/", (req, res) => {
@@ -9,3 +17,22 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000")
 })
+
+async function createUser() {
+  const newUser = await prisma.user.create({
+    data: {
+      name: "Rashel",
+      email: "abc@gmail.com",
+      password: "123",
+    }
+  });
+  console.log("User created successfully", newUser);
+}
+
+createUser()
+  .catch(e => {
+    console.log(e)
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  })
