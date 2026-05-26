5077ce76c1a301c078dcba13eedd148ba5732861
rashel9255 <ibrahim2000167@gmail.com>
2026-05-26 07:18:27 +0600

    create get all users and create user api

--- server.ts
+++ server.ts
@@ -4,6 +4,8 @@ import prisma from "./src/lib/db.js"
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Server is running 🚀")
 })
@@ -12,21 +14,25 @@ app.listen(3000, () => {
   console.log("Server running on http://localhost:3000")
 })
 
-async function createUser() {
-  const newUser = await prisma.user.create({
-    data: {
-      name: "Famous",
-      email: "efg@gmail.com",
-      password: "123",
-    }
-  });
-  console.log("User created successfully", newUser);
-}
+// GET: Fetch all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
 
-createUser()
-  .catch(e => {
-    console.log(e)
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  })
+// POST: Create a new user
+app.post("/users", async(req, res) => {
+    const { name, email, password } = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password
+      }
+    });
+    res.json(newUser);
+})
